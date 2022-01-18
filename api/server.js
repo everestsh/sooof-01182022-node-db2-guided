0824936a475d64c2c18612891fc9d30a747599ce
sooof <sooof@me.com>
2022-01-18 15:30:29 -0800

    Hello test

--- api/server.js
+++ api/server.js
@@ -8,6 +8,13 @@ server.use(express.json());
 
 server.use('/api/fruits', fruitsRouter);
 
+
+// http get :9000/hello
+server.get('/hello', (req, res) => {
+    res.status(200).json('hello world!!!!!!')
+  })
+
+//http get :9000
 server.use('*', (req, res)=>{
     res.status(404).json({
         message: 'not fount'
